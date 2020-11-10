27e8bfce2d743efd416f8a3b07fcae1bc6c2267e
Ahmed <imaneben8@gmail.com>
2020-11-10 21:52:30 +0000

    fix tests

--- __test__/user.spec.js
+++ __test__/user.spec.js
@@ -1,12 +1,13 @@
 import User from "../src/user"
 
 describe("Test Feature", () => {
-  let user;
+  let user = new User(thname,);
 
-  beforeEach(function () {
-    user = new User();
-  });
+  
   it('calctulates the age of today', () => {
     expect(user.getAge("July 21, 1992 01:15:00")).toEqual(28);
+  });
+  it('calctulates the date of the upcomimg year ', () => {
+    expect(user.getNextDob("1993-12-21")).toEqual("1993-12-21");
   });
 });
